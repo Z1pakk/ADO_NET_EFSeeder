30a40c833d0e1efff9626f112a4dcb4003caf774
Vlad <Vlad@DESKTOP-HJ9OVGE>
2020-02-20 20:29:13 +0200

    Add feature for new personal with photo.

--- PersonalsApp/MainWindow.xaml.cs
+++ PersonalsApp/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using PersonalsApp.Data.Services;
+using PersonalsApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PersonalsApp
 {
@@ -20,9 +13,39 @@ namespace PersonalsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private UserService _userService;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _userService = new UserService();
+        }
+
+        private void dgUsers_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<UserViewModel> users = new List<UserViewModel>();
+            foreach (var item in _userService.GetAll())
+            {
+                UserViewModel model = new UserViewModel()
+                {
+                    Id = item.Id,
+                    DateOfBirth = item.DateOfBirth,
+                    FirstName = item.FirstName,
+                    LastName = item.LastName,
+                    Phone = item.Phone,
+                    ImagePath = item.Image,
+                    Image = new BitmapImage(new System.Uri(item.Image.Contains("http") ? item.Image : Environment.CurrentDirectory + "//" + item.Image,UriKind.Absolute))
+                };
+                users.Add(model);
+            }
+            dgUsers.ItemsSource = users ;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddPersonalWindow dlg = new AddPersonalWindow();
+            dlg.ShowDialog();
         }
     }
 }
